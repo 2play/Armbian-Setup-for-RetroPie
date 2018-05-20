ae4ac1439c3f36cc2803c7c349a1cd73db1b2aa8
JyuHo <37599940+JyuHo@users.noreply.github.com>
2018-05-20 23:58:34 +0200

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,10 @@
+REGEX_VIDEO="\.avi\|\.mov\|\.mp4\|\.mkv\|\.3gp\|\.mpg\|\.mp3\|\.wav\|\.m4a\|\.a$
 REGEX_AUDIO="\.mp3\|\.wav\|\.m4a\|\.aac\|\.ogg\|\.flac"
 
+local rv="$REGEX_VIDEO"
 local ra="$REGEX_AUDIO"
 
+while pgrep omxplayer >/dev/null; do sleep 1; done
+mpv /home/$USER/RetroPie/splashscreens/*"$rv"
 (sleep 1; mpv --no-video /home/$USER/RetroPie/roms/music/*"$ra" >/dev/null 2>&1$
 emulationstation #auto
