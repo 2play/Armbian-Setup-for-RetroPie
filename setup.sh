22480cb39e51827677369d67854bf6d0a041e620
JyuHo <37599940+JyuHo@users.noreply.github.com>
2018-05-30 21:15:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,7 +54,7 @@ check_kernel () {
             unknown_os
         fi
         
-        if [[ "${kernel}" != "4.4.'>=119'-rockchip" ]] || [[ "4.14.'>=21'-rockchip" ]]; then
+        if [[ "${kernel}" != "4.4.119-rockchip" ]]; then
             echo "Detected kernel version as $kernel"
             unknown_os
         fi
@@ -65,14 +65,14 @@ check_kernel () {
 
 check_drivers () {
     if [[ ( -z "${drivers}" ) ]]; then
-        drivers=`cat /sys/module/midgard_kbase/version` || `cat /sys/module/mali_kbase/version`
+        drivers=`cat /sys/module/midgard_kbase/version`
         if [[ -z "$drivers" ]]; then
             unknown_os
         fi
         
         
         
-        if [[ "${drivers}" != "r14p0-01rel0 (UK version 10.6)" ]] || [[ "r19p0-01rel0 (UK version 10.6)" ]]; then
+        if [[ "${drivers}" != "r14p0-01rel0 (UK version 10.6)" ]]; then
             echo "Detected drivers version as $drivers"
             unknown_os
         fi
@@ -108,25 +108,22 @@ install () {
         echo "##  Installing various required packaged  ##"
         echo "############################################"
         echo ""
-        sudo apt install -y libtool cmake autoconf automake libxml2-dev libusb-1.0-0-dev libavcodec-dev libavformat-dev libavdevice-dev libdrm-dev pkg-config mpv
+        sudo apt install -y libtool cmake autoconf automake libxml2-dev libusb-1.0-0-dev libavcodec-dev \
+                            libavformat-dev libavdevice-dev libdrm-dev pkg-config mpv
         
         echo ""
         echo "#################################"
         echo "##  Installing kernel headers  ##"
         echo "#################################"
         echo ""
-        if [[ "${kernel}" == "4.14.'>=21'-rockchip" ]]; then
-            sudo apt install -y linux-headers-next-rockchip
-        else
-            sudo apt install -y linux-headers-rockchip
-        fi
+        sudo apt install -y linux-headers-rockchip
         echo ""
         echo "##############################################"
         echo "##  Installing requirements for GPU driver  ##"
         echo "##############################################"
         echo ""
-        sudo apt install -y libdrm2 libx11-6 libx11-data libx11-xcb1 libxau6 libxcb-dri2-0 libxcb1 libxdmcp6 libgles1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev
-        
+        sudo apt install -y libdrm2 libx11-6 libx11-data libx11-xcb1 libxau6 libxcb-dri2-0 libxcb1 \
+                            libxdmcp6 libgles1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev
         echo ""
         echo "#######################################"
         echo "##  Installing GPU userspace driver  ##"
@@ -212,8 +209,14 @@ install () {
         echo "##  Optionnal installation  ##"
         echo "##############################"
         echo ""
-        
-        if [[ "${kernel}" == "4.4.'>=119'-rockchip" ]]; then
+        read -p "Do you want to make optional installations, such as bluetooth, audio source, etc ...? (Y/N)" -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            read -p "Do you want installed bluetooth? (Y/N)" -n 1 -r
+            echo
+            if [[ $REPLY =~ ^[Yy]$ ]]
+            then
                 echo ""
                 echo "############################"
                 echo "##  Installing bluetooth  ##"
@@ -234,73 +237,86 @@ install () {
                 sudo systemctl daemon-reload
                 sudo systemctl stop tinker-bluetooth
                 sudo systemctl start tinker-bluetooth
-            
-         elif [[ "${kernel}" != "4.14.'>=21'-rockchip" ]]; then
+                
                 echo ""
-                echo "############################"
-                echo "##  Installing bluetooth  ##"
-                echo "############################"
-                echo ""
-                sudo apt install -y bluetooth
-                sudo sed -i "/ExecStart=/i\ExecStartPre=/usr/sbin/rfkill unblock all" /lib/systemd/system/tinker-bluetooth.service
-                sudo sed -i "/ExecStart=/a\Restart=on-failure" /lib/systemd/system/tinker-bluetooth.service
-                git clone https://github.com/JyuHo/tinkerboard_rtl8723bs.git
-                cd tinkerboard_rtl8723bs
-                make && sudo make install
-                sudo mv *.sh /usr/local/bin/
-                sudo sed -i "/ExecStart=/usr/bin/rtk_hciattach -n -s 115200 /dev/ttyS0 rtk_h5/c\ExecStart=/bin/bash /usr/local/bin/start_bt.sh" /lib/systemd/system/tinker-bluetooth.service
-                cd
-                rm -rf rtl8723bs_bt
-                echo "##  Bluetooth installed  ##"
-                echo ""
-                echo ""
-                echo "###############################"
-                echo "##  Launch bluetooth service ##"
-                echo "###############################"
-                echo ""
-                sudo systemctl stop tinker-bluetooth-restart
-                sudo systemctl disable tinker-bluetooth-restart
-                sudo rm /lib/systemd/system/tinker-bluetooth-restart.service
-                sudo systemctl daemon-reload
-                sudo systemctl stop tinker-bluetooth
-                sudo systemctl start tinker-bluetooth
-                echo "##  Bluetooth Started  ##"
-                echo ""
-        fi
-        read -p "Do you want to make optional installations, such as bluetooth, audio source, etc ...? (Y/N)" -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]
-        then
-             echo ""
-             echo "####################"
-             echo "##  Audio source  ##"
-             echo "####################"
-             echo ""
-             read -p "Do you want use audio by HDMI? (Y/N)" -n 1 -r
-             echo
-             if [[ $REPLY =~ ^[Yy]$ ]]
-             then
-                sudo sed -i "/defaults.pcm.card 0/c\defaults.pcm.card 1" /usr/share/alsa/alsa.conf
-                echo "##  Audio source on HDMI  ##"
+                echo "##  Bluetooth installed ##"
+                
+            else 
+                read -p "Do you want audio by HDMI? (Y/N)" -n 1 -r
+                echo
+                if [[ $REPLY =~ ^[Yy]$ ]]
+                then
+                    echo ""
+                    echo "####################"
+                    echo "##  Audio source  ##"
+                    echo "####################"
+                    echo ""
+                    sudo sed -i "/defaults.pcm.card 0/c\defaults.pcm.card 1" /usr/share/alsa/alsa.conf
+                    
+                    echo ""
+                    echo "##  Audio source on HDMI  ##"
              else
-                echo ""
-                echo "#####################################"
-                echo "##  Installing controller support  ##"
-                echo "#####################################"
-                echo ""
                 read -p "Do you want install Xbox One S Wireless support? (Y/N)" -n 1 -r
                 echo
-             if [[ $REPLY =~ ^[Yy]$ ]]
-             then
-                sudo sed -i "/nothing./a\echo 1 > /sys/module/bluetooth/parameters/disable_ertm &\n" /etc/rc.local
-                echo "##  Xbox One S support installed  ##"
-             fi
-             echo ""
-             echo "##############################"
-             echo "##  Installation completed  ##"
-             echo "##############################"
-             echo ""
-             echo "Run 'sudo ~/RetroPie-Setup/retropie_setup.sh' and then reboot your system. Then you can install the packages from RetroPie-Setup."
+                if [[ $REPLY =~ ^[Yy]$ ]]
+                then
+                    echo ""
+                    echo "#####################################"
+                    echo "##  Installing controller support  ##"
+                    echo "#####################################"
+                    echo ""
+                    sudo sed -i "/nothing./a\echo 1 > /sys/module/bluetooth/parameters/disable_ertm &\n" /etc/rc.local
+                    
+                    echo ""
+                    echo "##  Xbox One S support installed  ##"
+                
+                else
+                    read -p "Do you want install Background Music? (Y/N)" -n 1 -r
+                    echo
+                    if [[ $REPLY =~ ^[Yy]$ ]]
+                    then
+                        echo ""
+                        echo "#####################################"
+                        echo "##  Install Background Music  ##"
+                        echo "#####################################"
+                        echo ""
+                        mkdir -p $HOME/RetroPie/roms/musics
+                        sudo mkdir -p /opt/retropie/config/all
+                        sudo wget https://github.com/JyuHo/Armbian-Setup-for-RetroPie/blob/master/autostart.sh -O /opt/retropie/config/all/autostart.sh
+                        sudo wget https://github.com/JyuHo/Armbian-Setup-for-RetroPie/blob/master/runcommand-onend.sh -O /opt/retropie/config/all/runcommand-onend.sh
+                        sudo wget https://github.com/JyuHo/Armbian-Setup-for-RetroPie/blob/master/runcommand-onstart.sh -O /opt/retropie/config/all/runcommand-onstart.sh
+                        
+                        echo ""
+                        echo "##  Background Music ready  ##"
+                        echo "## You can drop your music files into ~/RetroPie/roms/musics"
+                    
+                    else
+                        read -p "Do you want install OMXPLAYER for splachscreen? (Y/N)" -n 1 -r
+                        echo
+                        if [[ $REPLY =~ ^[Yy]$ ]]
+                        then
+                            echo ""
+                            echo "#####################################"
+                            echo "##  Install OMXPLAYER  ##"
+                            echo "#####################################"
+                            echo ""
+                            wget http://ftp.de.debian.org/debian/pool/main/o/openssl/libssl1.0.0_1.0.2l-1~bpo8+1_armhf.deb
+                            sudo dpkg -i libssl1.0.0_1.0.2l-1~bpo8+1_armhf.deb
+                            sudo apt install libssh-4
+                            wget http://omxplayer.sconde.net/builds/omxplayer_0.3.7~git20170130~62fb580_armhf.deb
+                            sudo dpkg -i omxplayer_0.3.7~git20170130~62fb580_armhf.deb
+                            rm *.deb
+                            
+                            echo ""
+                            echo "##  OMXPLAYER installed  ##"
+                        
+                        else
+                            echo ""
+                            echo "##############################"
+                            echo "##  Installation completed  ##"
+                            echo "##############################"
+                            echo ""
+                            echo "Run 'sudo ~/RetroPie-Setup/retropie_setup.sh' and then reboot your system. Then you can install the packages from RetroPie-Setup."
     fi
 }
 
