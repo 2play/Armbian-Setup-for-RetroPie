9e25d212c6f549ebc17284b1d86fcc857404c388
JyuHo <37599940+JyuHo@users.noreply.github.com>
2018-06-03 15:05:09 +0200

    Update runcommand-onend.sh

--- runcommand-onend.sh
+++ runcommand-onend.sh
@@ -1,5 +1,1 @@
-REGEX_AUDIO="\.mp3\|\.wav\|\.m4a\|\.aac\|\.ogg\|\.flac"
-
-ra="$REGEX_AUDIO"
-
-mpv --no-video --loop --shuffle /home/$USER/RetroPie/roms/musics/*"$ra" >/dev/null 2>&1 &
+mpv --no-video --quiet -- no-display-video --loop --shuffle /home/$USER/RetroPie/roms/musics/*
