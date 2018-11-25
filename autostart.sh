db8a59e05de98bb44e950834c9ed5884c04d1ba1
2play <sprafs@gmail.com>
2018-11-25 22:54:44 +0100

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -4,7 +4,6 @@ REGEX_AUDIO="\.mp3\|\.wav\|\.m4a\|\.aac\|\.ogg\|\.flac"
 local rv="$REGEX_VIDEO" >/dev/null 2>&1 &
 local ra="$REGEX_AUDIO" >/dev/null 2>&1 &
 
-
 while pgrep omxplayer >/dev/null; do sleep 1; done
 mpv /home/$USER/RetroPie/splashscreens/*"$rv"
 sleep 1; mpv --no-video --no-display-video --loop --shuffle /home/$USER/RetroPie/roms/music/*"$ra" >/dev/null 2>&1 &
